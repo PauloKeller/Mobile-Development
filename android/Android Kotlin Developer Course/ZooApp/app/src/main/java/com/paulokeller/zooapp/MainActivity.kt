46961a5e688b38808f60e2c7971077c68e359ef6
PauloKeller <paulo.vinicius.keller@gmail.com>
2019-12-22 09:56:29 -0300

    zoo app done

--- android/Android Kotlin Developer Course/ZooApp/app/src/main/java/com/paulokeller/zooapp/MainActivity.kt
+++ android/Android Kotlin Developer Course/ZooApp/app/src/main/java/com/paulokeller/zooapp/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.paulokeller.zooapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
@@ -21,8 +22,18 @@ class MainActivity : AppCompatActivity() {
     animals.add(Animal("Tiger", "Tiger live in  big place with tree", R.drawable.white_tiger, true))
     animals.add(Animal("Zebra", "Zebra live in  big place with tree", R.drawable.zebra, false))
 
-    adapter = AnimalsAdapter(animals)
+    adapter = AnimalsAdapter(animals, this)
     tvListAnimal.adapter = adapter
+  }
+
+  fun delete(index:Int) {
+    animals.removeAt(index)
+    adapter!!.notifyDataSetChanged()
+  }
+
+  fun add (index: Int) {
+    animals.add(index, animals[index])
+    adapter!!.notifyDataSetChanged()
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,14 +43,38 @@ class MainActivity : AppCompatActivity() {
     loadAnimals()
   }
 
-  inner class AnimalsAdapter(private val list: ArrayList<Animal>) : BaseAdapter() {
+  inner class AnimalsAdapter(private val list: ArrayList<Animal>, private val context: Context) : BaseAdapter() {
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
       val animal = list[p0]
+      if (animal.isKiller == true) {
+        val myView = layoutInflater.inflate(R.layout.animal_killer_ticket, null)
+        myView.tvName.text = animal.name
+        myView.txDesc.text = animal.des
+        animal.image?.let { myView.ivAnimalImage.setImageResource(it) }
+        myView.ivAnimalImage.setOnClickListener {
+          val intent = Intent(context, AnimalInfoActivity::class.java)
+          intent.putExtra("name", animal.name)
+          intent.putExtra("des", animal.des)
+          intent.putExtra("image", animal.image)
+          startActivity(intent)
+        }
+
+        return myView
+      }
+
       val myView = layoutInflater.inflate(R.layout.animal_ticket, null)
       myView.tvName.text = animal.name
       myView.txDesc.text = animal.des
       animal.image?.let { myView.ivAnimalImage.setImageResource(it) }
+      myView.ivAnimalImage.setOnClickListener {
+        val intent = Intent(context, AnimalInfoActivity::class.java)
+        intent.putExtra("name", animal.name)
+        intent.putExtra("des", animal.des)
+        intent.putExtra("image", animal.image)
+        startActivity(intent)
+      }
+
       return myView
     }
 
